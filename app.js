7bb0832cdc95f8a319117330dc2c3e3cdc6c706e
upasanagithub <ups.win@gmail.com>
2016-10-01 15:40:21 +0530

    committed

---
+++ app.js
@@ -0,0 +1,21 @@
+
+  var app=angular.module("app",[]);
+  app.controller("myController",function($scope){
+    $scope.val="ups";
+  $scope.count=function(){
+
+    var str = $scope.items.toString();
+    str = str.split(",");
+    if($scope.items.value==null){
+        $scope.msg = "Please enter data first";
+    }
+    if(str.length<=3 && $scope.items.length!=0){
+        $scope.msg = "Enjoy!";
+    }
+    if(str.length>3){
+        $scope.msg = "Too much!";
+    }
+
+
+  } ;
+  });

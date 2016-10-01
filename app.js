81165b8c17194a278c70362db3b29ac38920bc9e
upasanagithub <ups.win@gmail.com>
2016-10-01 17:14:03 +0530

    COmmitted

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 
-  var app=angular.module("app",[]);
-  app.controller("myController",function($scope){
-  
+(function(){  var app=angular.module("app",[]);
+  app.controller("myController",myController);
+  myController.$inject=["$scope"];
+  function myController($scope){
+  $scope.items = "";
+  $scope.msg = "";
   $scope.count=function(){
 
     var str = $scope.items.toString();
     str = str.split(",");
-    if($scope.items.value==null){
+    if($scope.items.length==0){
         $scope.msg = "Please enter data first";
     }
     if(str.length<=3 && $scope.items.length!=0){
@@ -18,4 +21,5 @@
 
 
   } ;
-  });
+  }
+})();
